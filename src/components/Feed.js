e387ccb185ce6ed979ecff3802fee8f1a8f9ee54
Umesh <umeshmane575@gmail.com>
2024-05-12 09:51:48 +0530

    Complete miniprofile component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -13,7 +13,10 @@ export default function Feed() {
             </section>
             {/* MiniProfile right */}
             <section className='hidden md:inline-grid md:col-span-1'>
-                <MiniProfile />
+             <div className='fixed w-[380px]'>
+             <MiniProfile />
+             </div>
+               
             </section>
 
         </main>
--- src/components/MiniProfile.js
+++ src/components/MiniProfile.js
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function MiniProfile() {
-  return (
-    <div>
-      miniprofile
-    </div>
-  )
+    const { data: session } = useSession();
+    return (
+        <div className='flex items-center justify-between mt-14 scroll-ml-10'>
+            <img
+                src={session?.user?.image || '/800px-Instagram_logo_2016.webp'}
+                alt='user profile img or instagram logo'
+                className='w-16 h-16 rounded-full border p-[2px]'
+            />
+            <div className='flex-1 ml-4'>
+                <h2 className='font-bold'>{session?.user?.username}</h2>
+                <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
+            </div>
+
+                {session ? (
+                    <button
+                        onClick={signOut}
+                        className='text-blue-500 text-sm font-semibold'
+                    >
+                        Sign out
+                    </button>
+
+                ) : (
+                    <button
+                        onClick={signIn}
+                        className='text-blue-500 text-sm font-semibold'
+                    >
+                        Sign In
+                    </button>
+
+                )}
+        </div>
+    )
 }
